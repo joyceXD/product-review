90bc6147339012b04297258f2dac1869b630de56
Pang, Xin <xin.pang@philips.com>
2017-09-08 00:55:32 +0200

    Data loading to MongoDB.

---
+++ scripts/test_read_data.py
@@ -0,0 +1,72 @@
+import gzip
+import json
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+import time
+
+START_TIME = 1388534400
+END_TIME = 1404172800
+
+
+def parse(path):
+    g = gzip.open(path, 'r')
+    for l in g:
+        yield eval(l)
+
+
+def get_df(path):
+    i = 0
+    df = {}
+    for d in parse(path):
+        df[i] = d
+        i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
+
+
+def insert_item_metadata(meta_collection, meta_path):
+    item_meta_data = []
+
+    for item_meta in parse(meta_path):
+        if len(item_meta_data) < 1000:
+            item_meta_data.append(item_meta)
+        else:
+            meta_collection.insert_many(item_meta_data)
+            item_meta_data = []
+
+    if not meta_collection:
+        meta_collection.insert_many(item_meta_data)
+
+
+def insert_item_review(review_collection, review_path):
+    item_reviews = []
+
+    for review in parse(review_path):
+        if 'unixReviewTime' in review.keys():
+            if START_TIME < review['unixReviewTime'] < END_TIME:
+                if len(item_reviews) < 1000:
+                    item_reviews.append(review)
+                else:
+                    review_collection.insert_many(item_reviews)
+                    item_reviews = []
+
+    if not item_reviews:
+        review_collection.insert_many(item_reviews)
+
+
+start_time = time.time()
+
+# path = 'https://s3-eu-west-1.amazonaws.com/bigdata-team/job-interview/metadata.json.gz'
+review_data_path = '../data/item_dedup.json.gz'
+meta_data_path = '../data/metadata.json.gz'
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client.product
+meta_collection = db.metadata
+review_collection = db.review
+
+insert_item_review(review_collection, review_data_path)
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
